276bb094afafdc2d4e105f26fbf595d1be585830
JoeriDekker <joeridekker2002@gmail.com>
2023-07-04 13:08:07 +0200

    collection functions + cleanup

--- Apotheek/apotheek/base/views.py
+++ Apotheek/apotheek/base/views.py
@@ -13,23 +13,22 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 
 
-def say_hello(request):
-    context = {"first_name": "Joeri", "last_name": "Dekker"}
-    return render(request, "base/hello.html", context)
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+        
+    context = {"form": form}
+
+    return render(request, "registration/register.html", context)
 
 def index(request):
     return render(request, "base/index.html")
-
-def nameform(requests):
-    form = NameForm()
-    context = {"form": form}
-
-    if requests.method == "POST":
-        name = requests.POST.get("your_name")
-        context["greeting"] = f"Welcome {name}!"
-
-    return render(requests, "base/nameform.html", context)
-
 
 @login_required
 def profile(request):
@@ -108,3 +107,68 @@ def delete_medicine(request, medicine_id):
     medicine = Medicine.objects.get(pk=medicine_id)
     medicine.delete()
     return redirect('medicines')
+
+
+def collections(request):
+    collections_list = Collection.objects.all()
+    context = {
+        'collections': collections_list
+    }
+    return render(request, 'base/collections.html', context)
+
+def add_collection(request):
+    if request.method == 'POST':
+        form = CollectionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('collections')
+    else:
+        form = CollectionForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'base/add_collection_item.html', context)
+
+def delete_collection(request, collection_id):
+    collection = Collection.objects.get(pk=collection_id)
+    collection.delete()
+    return redirect('collections')
+
+def personal_collections(request):
+    collections = Collection.objects.filter(user=request.user, collected=False)
+    context = {
+        'collections': collections
+    }
+    return render(request, 'base/personal_collections.html', context)
+
+def collect(request, collection_id):
+    collection = Collection.objects.get(pk=collection_id)    
+    collection.collected = True
+    collection.save()
+    return redirect('personal_collections')
+
+def collected_items(request):
+    collected_items = Collection.objects.filter(collected=True)
+    context = {
+        'collected_items': collected_items
+    }
+    return render(request, 'base/collected_items.html', context)
+
+def approve_collected_item(request, item_id):
+    item = Collection.objects.get(pk=item_id)
+    item.collected_approved = True
+    item.collected_approved_by = request.user
+    item.save()
+    return redirect('collected_items')
+
+def nameform(requests):
+    form = NameForm()
+    context = {"form": form}
+
+    if requests.method == "POST":
+        name = requests.POST.get("your_name")
+        context["greeting"] = f"Welcome {name}!"
+
+    return render(requests, "base/nameform.html", context)
