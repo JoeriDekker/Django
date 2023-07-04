dcae31425901bd43161543dd51ba62776112880e
joelhogeschool <124694056+joelhogeschool@users.noreply.github.com>
2023-07-04 13:03:32 +0200

    first update
    pushing the first couple of pages

--- Apotheek/apotheek/base/urls.py
+++ Apotheek/apotheek/base/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("hello/", views.say_hello, name="hello"),
     path("nameform/", views.nameform, name="nameform"),
+    path("profile/", views.profile, name="profile"),
+    path('update_profile/', views.update_profile, name='update_profile'),
+    path('medicines/', views.medicines, name='medicines'),
+    path('add_medicine/', views.add_medicine, name='add_medicine'),
+    path('update_medicine/<int:medicine_id>/', views.update_medicine, name='update_medicine'),
+    path('delete_medicine/<int:medicine_id>/', views.delete_medicine, name='delete_medicine'),
 ]
--- Apotheek/apotheek/base/views.py
+++ Apotheek/apotheek/base/views.py
@@ -29,3 +29,82 @@ def nameform(requests):
         context["greeting"] = f"Welcome {name}!"
 
     return render(requests, "base/nameform.html", context)
+
+
+@login_required
+def profile(request):
+    profile = request.user.profile
+    context = {"profile": profile}
+    return render(request, "base/profile.html", context)
+
+@login_required
+def update_profile(request):
+    user = request.user
+    profile = user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=profile)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'base/update_profile.html', context)
+
+@login_required
+@staff_member_required
+def medicines(request):
+    medicines = Medicine.objects.all()
+    context = {
+        'medicines': medicines
+    }
+    return render(request, 'base/medicines.html', context)
+
+@login_required
+@staff_member_required
+def add_medicine(request):
+    if request.method == 'POST':
+        form = MedicineForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('medicines')
+    else:
+        form = MedicineForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'base/add_medicine.html', context)
+
+@login_required
+@staff_member_required
+def update_medicine(request, medicine_id):
+    medicine = Medicine.objects.get(pk=medicine_id)
+
+    if request.method == 'POST':
+        form = MedicineForm(request.POST, instance=medicine)
+        if form.is_valid():
+            form.save()
+            return redirect('medicine_list')
+    else:
+        form = MedicineForm(instance=medicine)
+
+    context = {
+        'form': form,
+        'medicine': medicine
+    }
+
+    return render(request, 'base/update_medicine.html', context)
+
+@login_required
+@staff_member_required
+def delete_medicine(request, medicine_id):
+    medicine = Medicine.objects.get(pk=medicine_id)
+    medicine.delete()
+    return redirect('medicines')
